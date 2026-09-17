a15d47aba6e7c5605bbacc577f3f8546c9568f87
huyle <huyle@ftech.ltd>
2026-09-17 22:41:49 +0700

    feat(webapp): migrate to SAP Fiori Elements V4 ListReport and ObjectPage

--- app/webapp/Component.js
+++ app/webapp/Component.js
@@ -1,12 +1,11 @@
-sap.ui.define(
-    ["sap/fe/core/AppComponent"],
-    function (AppComponent) {
-        "use strict";
+sap.ui.define([
+    "sap/fe/core/AppComponent"
+], function (AppComponent) {
+    "use strict";
 
-        return AppComponent.extend("sap.payment.app.Component", {
-            metadata: {
-                manifest: "json"
-            }
-        });
-    }
-);
+    return AppComponent.extend("sap.payment.app.Component", {
+        metadata: {
+            manifest: "json"
+        }
+    });
+});
